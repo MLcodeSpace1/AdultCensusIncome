4f03cd5ea3197161b6594be944bc97de4c7c6659
MLcodeSpace1 <120629320+MLcodeSpace1@users.noreply.github.com>
2022-12-16 10:20:38 +0000

    adultIncomeClassification.py

--- adult/entity/adultIncomeClassif.py
+++ adult/entity/adultIncomeClassif.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from adult.exception import adultException
+from adult.exception import AdultException
 from adult.util.util import load_object
 
 import pandas as pd
@@ -26,18 +26,22 @@ class adultData:
                  country:str
                  ):
         try:
-            self.longitude = longitude
-            self.latitude = latitude
-            self.adult_median_age = adult_median_age
-            self.total_rooms = total_rooms
-            self.total_bedrooms = total_bedrooms
-            self.population = population
-            self.households = households
-            self.median_income = median_income
-            self.ocean_proximity = ocean_proximity
-            self.median_house_value = median_house_value
+            self.age = age
+            self.workclass = workclass
+            self.fnlwgt = fnlwgt
+            self.education = education
+            self.education-num = education-num
+            self.marital-status = marital-status
+            self.occupation = occupation
+            self.relationship = relationship
+            self.race = race
+            self.sex = sex
+            self.capital-gain = capital-gain
+            self.capital-loss = capital-loss
+            self.hours-per-week = hours-per-week
+            self.country = country
         except Exception as e:
-            raise adultException(e, sys) from e
+            raise AdultException(e, sys) from e
 
     def get_adult_input_data_frame(self):
 
@@ -45,32 +49,37 @@ class adultData:
             adult_input_dict = self.get_adult_data_as_dict()
             return pd.DataFrame(adult_input_dict)
         except Exception as e:
-            raise adultException(e, sys) from e
+            raise AdultException(e, sys) from e
 
     def get_adult_data_as_dict(self):
         try:
             input_data = {
-                "longitude": [self.longitude],
-                "latitude": [self.latitude],
-                "adult_median_age": [self.adult_median_age],
-                "total_rooms": [self.total_rooms],
-                "total_bedrooms": [self.total_bedrooms],
-                "population": [self.population],
-                "households": [self.households],
-                "median_income": [self.median_income],
-                "ocean_proximity": [self.ocean_proximity]}
+                "age": [self.age],
+                "workclass": [self.workclass],
+                "fnlwgt": [self.fnlwgt],
+                "education": [self.education],
+                "education-num": [self.education-num],
+                "marital-status": [self.marital-status],
+                "occupation": [self.occupation],
+                "relationship": [self.relationship],
+                "race": [self.race],
+                "sex": [self.sex],
+                "capital-gain": [self.capital-gain],
+                "capital-loss": [self.capital-loss],
+                "hours-per-week": [self.hours-per-week],
+                "country": [self.country]}
             return input_data
         except Exception as e:
-            raise adultException(e, sys)
+            raise AdultException(e, sys)
 
 
-class adultPredictor:
+class adultClassifier:
 
     def __init__(self, model_dir: str):
         try:
             self.model_dir = model_dir
         except Exception as e:
-            raise adultException(e, sys) from e
+            raise AdultException(e, sys) from e
 
     def get_latest_model_path(self):
         try:
@@ -80,13 +89,13 @@ class adultPredictor:
             latest_model_path = os.path.join(latest_model_dir, file_name)
             return latest_model_path
         except Exception as e:
-            raise adultException(e, sys) from e
+            raise AdultException(e, sys) from e
 
     def predict(self, X):
         try:
             model_path = self.get_latest_model_path()
             model = load_object(file_path=model_path)
-            median_house_value = model.predict(X)
-            return median_house_value
+            Incomevalue = model.predict(X)
+            return Incomevalue
         except Exception as e:
-            raise adultException(e, sys) from e
+            raise AdultException(e, sys) from e
